1a6bf6f91ce6ddb8eb619175a780c27a8fabf19b
Yaya Awad <yehiakawad@gmail.com>
2024-01-05 22:09:05 -0500

    Plus One

--- Easy/Array/Array.py
+++ Easy/Array/Array.py
@@ -130,6 +130,23 @@ class Array(object):
             nums[j] = 0
             j+=1
     
+    # Plus One 
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits) - 1
+        
+        while digits[n] == 9:
+            digits[n] = 0
+            n = n - 1
+        if n < 0:
+            digits = [1] + digits
+        else:
+            digits[n] = digits[n] + 1
+        return digits
+            
 
 
     
