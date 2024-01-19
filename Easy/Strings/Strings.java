32ad1a44f518994ac7d9da118ba6957fc3fe433a
Yaya Awad <yehiakawad@gmail.com>
2024-01-19 16:08:45 -0500

    leet reverse string

--- Easy/Strings/Strings.java
+++ Easy/Strings/Strings.java
@@ -1,5 +1,29 @@
 package Easy.Strings;
 
 public class Strings {
+    public Strings() {
 
+    }
+
+    public static void main(String[] args) {
+        Strings a = new Strings();
+
+        String x = "Hello world";
+        a.reverseString(x.toCharArray());
+        int g = 0;
+    }
+
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
 }
